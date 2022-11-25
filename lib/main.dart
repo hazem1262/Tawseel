88a0d37a1eb0e91c7e77d4ddca8f43a8863b2f4b
Hazem Ashraf <hazem.ashraf@omniful.com>
2022-11-25 11:14:56 +0200

    add firebase dependancies

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -10,7 +11,7 @@ import 'features/address/models/AddressDetailsScreen.dart';
 
 void main() async {
   await initAppDependencies();
-
+  await Firebase.initializeApp();
   runApp(
     EasyLocalization(
       supportedLocales: [Locale('en'), Locale('ar')],
